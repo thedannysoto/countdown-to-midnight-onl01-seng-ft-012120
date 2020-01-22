9457ef43b4cdf510da8922a660ee9935b12c89e9
Danny Soto <87dsoto@gmail.com>
2020-01-22 04:02:21 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -5,3 +5,13 @@ def countdown(x)
   end
   return "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(x)
+  while x > 0
+    puts "#{x} SECOND(S)!"
+    x -= 1
+    sleep(1)
+  end
+  return "HAPPY NEW YEAR!"
+  
+end
